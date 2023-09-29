89e1622619fc1456ecc252ae55803a1bd9033cb6
theopin <theopinto98@hotmail.com>
2023-09-29 21:41:20 +0800

    Integrate db connection to app

--- express/src/database/mongodb/mongodb.ts
+++ express/src/database/mongodb/mongodb.ts
@@ -4,17 +4,15 @@ import mongoose from 'mongoose'
 const newDocumentConfig = { new: true }
 
 class MongoDb implements Database {
+  schema: mongoose.Schema
+  ModelObject: any
+
   constructor (schemaLayout: any, modelName: string) {
     this.schema = new mongoose.Schema(schemaLayout)
     this.ModelObject = mongoose.model(modelName, this.schema)
   }
 
-  schema = new mongoose.Schema({ name: String, size: String })
-  ModelObject = mongoose.model('Tank', this.schema)
-
   async connect (): Promise<void> {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test')
-
     mongoose.connection.on('connected', () => {
       console.log('Connected to MongoDB database successfully!')
     })
@@ -23,6 +21,8 @@ class MongoDb implements Database {
       console.error(err)
       throw new Error('Failed to connect to MongoDB database!')
     })
+
+    await mongoose.connect('mongodb://127.0.0.1:27017')
   }
 
   async disconnect (): Promise<void> {
@@ -31,23 +31,23 @@ class MongoDb implements Database {
 
   async createEntity (data: any): Promise<any> {
     const collection = new this.ModelObject(data)
-    return await collection.save()
+    return collection.save()
   }
 
   async getOneEntity (id: string): Promise<any> {
-    return await this.ModelObject.findOne({ id }).exec()
+    return this.ModelObject.findOne({ id }).exec()
   }
 
   async getAllEntities (params: Partial<any>): Promise<any> {
-    return await this.ModelObject.find({ params }).exec()
+    return this.ModelObject.find({ params }).exec()
   }
 
   async updateEntity (id: string, newData: Partial<any>): Promise<any> {
-    return await this.ModelObject.findOneAndUpdate({ id }, newData, newDocumentConfig).exec()
+    return this.ModelObject.findOneAndUpdate({ id }, newData, newDocumentConfig).exec()
   }
 
   async deleteEntity (id: string): Promise<any> {
-    return await this.ModelObject.deleteOne({ id })
+    return this.ModelObject.deleteOne({ id })
   }
 }
 
--- express/src/index.ts
+++ express/src/index.ts
@@ -2,11 +2,16 @@ import express, { type Express } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import EntityRouter from './routes/entity.route'
+import type Database from './database/interface/database.interface'
+import MongoDb from './database/mongodb/mongodb'
 
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT ?? 4000
+const database: Database = new MongoDb({ num: 'number' }, 'Tank')
+
+database.connect()
 
 app.use(cors()) // config cors so that front-end can use
 app.options('*', cors())
