4988807aea9d99ae81fc863ee1d8fa41cc76b495
theopin <theopinto98@hotmail.com>
2023-09-29 12:54:40 +0800

    Convert express project to typescript

--- express/index.ts
+++ express/index.ts
@@ -1,14 +1,15 @@
-import express from 'express';
+import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const app = express();
+const app: Express = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
+
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
