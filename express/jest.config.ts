3e7927f5df2d47c7ba78c337b2c611587ae3843f
theopin <theopinto98@hotmail.com>
2023-10-01 12:02:40 +0800

    Fix jest test match

--- express/jest.config.ts
+++ express/jest.config.ts
@@ -3,9 +3,9 @@ export default {
   coverageProvider: 'v8',
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
 
-  roots: ['<rootDir>/test'],
+  roots: ['test'],
 
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testMatch: ['__test/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest'
   }
