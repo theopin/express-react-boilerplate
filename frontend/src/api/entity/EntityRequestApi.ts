f93bc5feba0f3182a5ec594075bf3526c0a44b5d
theopin <theopinto98@hotmail.com>
2023-11-25 20:24:54 +0800

    Update some stuff

--- frontend/src/api/entity/EntityRequestApi.ts
+++ frontend/src/api/entity/EntityRequestApi.ts
@@ -3,29 +3,29 @@ import { type EntityFilter } from '../../models/user/EntityFilter'
 import { RequestTypes } from '../utils/RequestTypes'
 
 const createNewEntity = async (entityDetails: Entity): Promise<any> => {
-  return await RequestTypes.postRequest('/', entityDetails)
+  return await RequestTypes.postRequest('/backend/', entityDetails)
 }
 
 const getEntities = async (): Promise<any> => {
-  return await RequestTypes.getRequest('/')
+  return await RequestTypes.getRequest('/backend/')
 }
 
 const getEntityById = async (entityId: number): Promise<any> => {
-  return await RequestTypes.getRequest(`/${entityId}`)
+  return await RequestTypes.getRequest(`/backend/${entityId}`)
 }
 
 const getEntitiesByFilter = async (filterParams: EntityFilter): Promise<any> => {
-  return await RequestTypes.getRequest('/', {
+  return await RequestTypes.getRequest('/backend/', {
     params: filterParams
   })
 }
 
 const updateEntityById = async (id: string, updatedDetails: EntityFilter): Promise<any> => {
-  return await RequestTypes.patchRequest(`/${id}`, updatedDetails)
+  return await RequestTypes.patchRequest(`/backend/${id}`, updatedDetails)
 }
 
 const deleteEntityById = async (id: string): Promise<any> => {
-  return await RequestTypes.deleteRequest(`/${id}`)
+  return await RequestTypes.deleteRequest(`/backend/${id}`)
 }
 
 export const EntityRequestApi = {
