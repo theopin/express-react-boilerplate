ed967efbf6b3350418bb1a55a37a8bb54e95fcbd
theopin <theopinto98@hotmail.com>
2023-12-10 17:19:41 +0800

    Fix issue with postgres integration

--- data/users.sql
+++ data/users.sql
@@ -1,57 +1,78 @@
+CREATE OR REPLACE FUNCTION generate_object_id() RETURNS varchar AS $$
+    DECLARE
+        time_component bigint;
+        machine_id bigint := FLOOR(random() * 16777215);
+        process_id bigint;
+        seq_id bigint := FLOOR(random() * 16777215);
+        result varchar:= '';
+    BEGIN
+        SELECT FLOOR(EXTRACT(EPOCH FROM clock_timestamp())) INTO time_component;
+        SELECT pg_backend_pid() INTO process_id;
+
+        result := result || lpad(to_hex(time_component), 8, '0');
+        result := result || lpad(to_hex(machine_id), 6, '0');
+        result := result || lpad(to_hex(process_id), 4, '0');
+        result := result || lpad(to_hex(seq_id), 6, '0');
+        RETURN result;
+    END;
+$$ LANGUAGE PLPGSQL;
+
+DROP TABLE IF EXISTS users;
+
 create table users (
-	id INT,
-	username VARCHAR(50),
-	p_name VARCHAR(50),
-	email VARCHAR(50),
-	lang VARCHAR(50)
+	_id varchar(24) NOT NULL DEFAULT generate_object_id(),
+	username VARCHAR(50) PRIMARY KEY,
+	password VARCHAR(100) NOT NULL,
+	email VARCHAR(50) NOT NULL
 );
-insert into users (id, username, p_name, email, lang) values (1, 'stryme0', 'Sasha', 'sbenford0@jalbum.net', 'Catalan');
-insert into users (id, username, p_name, email, lang) values (2, 'dskoate1', 'Dredi', 'dclemencon1@fema.gov', 'Kazakh');
-insert into users (id, username, p_name, email, lang) values (3, 'dbedells2', 'Dore', 'dpillinger2@fema.gov', 'Lao');
-insert into users (id, username, p_name, email, lang) values (4, 'msalling3', 'Michelina', 'msilverton3@mlb.com', 'Pashto');
-insert into users (id, username, p_name, email, lang) values (5, 'tmcwhan4', 'Tildie', 'tkindell4@time.com', 'Polish');
-insert into users (id, username, p_name, email, lang) values (6, 'clarcher5', 'Conni', 'crowcastle5@macromedia.com', 'Zulu');
-insert into users (id, username, p_name, email, lang) values (7, 'tfishly6', 'Tamarah', 'ttrevna6@microsoft.com', 'Persian');
-insert into users (id, username, p_name, email, lang) values (8, 'msuston7', 'Minta', 'mgreaser7@hc360.com', 'Thai');
-insert into users (id, username, p_name, email, lang) values (9, 'dcleef8', 'Darcey', 'dyosselevitch8@wordpress.com', 'Afrikaans');
-insert into users (id, username, p_name, email, lang) values (10, 'gguiver9', 'Gavra', 'gvasilov9@adobe.com', 'Tetum');
-insert into users (id, username, p_name, email, lang) values (11, 'rsilverwooda', 'Ruprecht', 'rnestora@gov.uk', 'Malayalam');
-insert into users (id, username, p_name, email, lang) values (12, 'abastoneb', 'Almeta', 'arennenbachb@dedecms.com', 'Finnish');
-insert into users (id, username, p_name, email, lang) values (13, 'calbersc', 'Clive', 'ckarpmanc@quantcast.com', 'Moldovan');
-insert into users (id, username, p_name, email, lang) values (14, 'njasikd', 'Nester', 'nharrodd@walmart.com', 'Marathi');
-insert into users (id, username, p_name, email, lang) values (15, 'honowlane', 'Hartwell', 'hcoleye@vinaora.com', 'Fijian');
-insert into users (id, username, p_name, email, lang) values (16, 'lbolingbrokef', 'Lyndsey', 'lcrannagef@devhub.com', 'New Zealand Sign Language');
-insert into users (id, username, p_name, email, lang) values (17, 'aseakingg', 'Ashien', 'aarmorg@cornell.edu', 'Belarusian');
-insert into users (id, username, p_name, email, lang) values (18, 'frosinah', 'Farris', 'fpierrepointh@mlb.com', 'Finnish');
-insert into users (id, username, p_name, email, lang) values (19, 'mgoddardi', 'Minette', 'mmallardi@wunderground.com', 'Gagauz');
-insert into users (id, username, p_name, email, lang) values (20, 'dgallawayj', 'Dominique', 'dmcconaghyj@china.com.cn', 'Irish Gaelic');
-insert into users (id, username, p_name, email, lang) values (21, 'rcaulierk', 'Rikki', 'rhatfieldk@csmonitor.com', 'Czech');
-insert into users (id, username, p_name, email, lang) values (22, 'jthornhilll', 'Jeane', 'jtomsettl@123-reg.co.uk', 'Tetum');
-insert into users (id, username, p_name, email, lang) values (23, 'lmackilpatrickm', 'Liuka', 'lebdenm@mysql.com', 'Korean');
-insert into users (id, username, p_name, email, lang) values (24, 'lnialsn', 'Leelah', 'lbennisonn@theguardian.com', 'West Frisian');
-insert into users (id, username, p_name, email, lang) values (25, 'mhadwicko', 'Marlin', 'malveyo@1688.com', 'Armenian');
-insert into users (id, username, p_name, email, lang) values (26, 'nantonomolip', 'Nicola', 'nstoodleyp@yahoo.co.jp', 'Kannada');
-insert into users (id, username, p_name, email, lang) values (27, 'sloftingq', 'Selma', 'seddinsq@google.co.uk', 'Somali');
-insert into users (id, username, p_name, email, lang) values (28, 'gortmannr', 'Gleda', 'glegonidecr@patch.com', 'Aymara');
-insert into users (id, username, p_name, email, lang) values (29, 'bmincis', 'Brita', 'bgazeys@cocolog-nifty.com', 'Assamese');
-insert into users (id, username, p_name, email, lang) values (30, 'amccuet', 'Armando', 'aguyet@salon.com', 'Tetum');
-insert into users (id, username, p_name, email, lang) values (31, 'kbrandoneu', 'Kipp', 'kbearwardu@sfgate.com', 'Fijian');
-insert into users (id, username, p_name, email, lang) values (32, 'dclimsonv', 'Dot', 'dsidnallv@senate.gov', 'Swati');
-insert into users (id, username, p_name, email, lang) values (33, 'ealsobrookw', 'Emma', 'espellicyw@cnn.com', 'Lao');
-insert into users (id, username, p_name, email, lang) values (34, 'larensonx', 'Lauren', 'lallisonx@icio.us', 'Mongolian');
-insert into users (id, username, p_name, email, lang) values (35, 'dgullefanty', 'Dwight', 'djellicoy@blogtalkradio.com', 'Gagauz');
-insert into users (id, username, p_name, email, lang) values (36, 'astubbingsz', 'Avrit', 'agebhardtz@jigsy.com', 'Tamil');
-insert into users (id, username, p_name, email, lang) values (37, 'lmccusker10', 'Lise', 'ldifrancecshi10@4shared.com', 'Arabic');
-insert into users (id, username, p_name, email, lang) values (38, 'csudy11', 'Chane', 'cdavidovitch11@1688.com', 'Polish');
-insert into users (id, username, p_name, email, lang) values (39, 'rwestwood12', 'Ravi', 'rswindells12@shutterfly.com', 'Ndebele');
-insert into users (id, username, p_name, email, lang) values (40, 'rarrol13', 'Royall', 'rpenticost13@skyrock.com', 'Latvian');
-insert into users (id, username, p_name, email, lang) values (41, 'lhorney14', 'Laney', 'lfowles14@printfriendly.com', 'Fijian');
-insert into users (id, username, p_name, email, lang) values (42, 'gwhitington15', 'Gerri', 'gturnock15@nhs.uk', 'Japanese');
-insert into users (id, username, p_name, email, lang) values (43, 'jcaro16', 'Jill', 'jbridgwood16@ehow.com', 'Zulu');
-insert into users (id, username, p_name, email, lang) values (44, 'ukennicott17', 'Ursola', 'ulamball17@craigslist.org', 'Hebrew');
-insert into users (id, username, p_name, email, lang) values (45, 'lneissen18', 'Lennie', 'lcrutch18@abc.net.au', 'Arabic');
-insert into users (id, username, p_name, email, lang) values (46, 'othomson19', 'Orlan', 'ostother19@howstuffworks.com', 'Afrikaans');
-insert into users (id, username, p_name, email, lang) values (47, 'aloukes1a', 'Angy', 'aorpen1a@ning.com', 'Irish Gaelic');
-insert into users (id, username, p_name, email, lang) values (48, 'bbabin1b', 'Boigie', 'bdansken1b@about.me', 'Lao');
-insert into users (id, username, p_name, email, lang) values (49, 'tgrunguer1c', 'Toddie', 'tmorrison1c@answers.com', 'Tamil');
-insert into users (id, username, p_name, email, lang) values (50, 'tmaddock1d', 'Tommy', 'tsummerton1d@apple.com', 'Swahili');
+
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20eb', 'Wilek', '$2a$04$gFV8SwSMf9jzfx5Gax2x9eJ/o1dtxa3WMbMxbbDvN2cpzhHdn/KFy', 'wlannin0@admin.ch');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20ec', 'Neville', '$2a$04$VrmVSPIcSEgMV/kW6yo/GugiuIWDNAHYQEky0X6ic/eYyOrggKdMy', 'nlawlor1@dailymotion.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20ed', 'Alic', '$2a$04$y4G4V3/gvvBijJ0HzdqtGuSuTf4bCz3ucOsEz24T3oUtLFTquLaLG', 'astokoe2@hc360.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20ee', 'Noellyn', '$2a$04$uqmJvgdRxfS5amtuZu3VZu4ftuZZV3hMGXIX6ze/2VaagJ/E14vCW', 'nadame3@alibaba.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20ef', 'Richy', '$2a$04$fwZsy.IEwxTV9i8zIKpEGOn1xO5eWhIL8V9KefJN9Q5bxVQOtVQO6', 'rtesmond4@etsy.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20f0', 'Marena', '$2a$04$WxXUU3Ni9oX5xrvpqM/RlOq5d17x8y5Gz3.H/LgZ0DmHXJpc5M.YK', 'mgoodright5@dagondesign.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20f1', 'Bernice', '$2a$04$zmDsdhLN1ZhRqfcBli7K8ualPeOC1efVDp04XiolFg81rOUO/jL3a', 'brosenfarb6@dell.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20f2', 'Audra', '$2a$04$wV9njvlAIh118jeBSDlkjeo4jkedwUTblbrpSAOHjNnTpXa4uIhBa', 'agillan7@epa.gov');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20f3', 'Nicholle', '$2a$04$iBN1MGyUKBB8Prj52BX..eYA3lX8hUQlxENBwRMih8Ax81whJXuaW', 'ncheetham8@so-net.ne.jp');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20f4', 'Malachi', '$2a$04$7ipQpGevDpXX4/BjCR0o9OZESlcc4ucZjhYj/6pHvDakNh21lVwYa', 'mleyman9@youku.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20f5', 'Chuck', '$2a$04$OehNrsga3sascvMzjZYRzu5vA7/sodhrsukW0G7B4klqhUsUaWovG', 'cmarta@hostgator.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20f6', 'Ulberto', '$2a$04$meeB5mXstw.agV5IbvSnWuxDcENlHoZA9V48ApMvLZ7n4wMb4eupG', 'ubrackb@time.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20f7', 'Christoffer', '$2a$04$Tpovq/U29YzAiNUWL.u9JO9FJYs8UkbXo.k6loJ6QSEFMKIKQX67a', 'cbrocktonc@acquirethisname.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20f8', 'Consuela', '$2a$04$muGIZQg5kWD1sH4Cdq9XtuTdRLMpGsaxf12Y2hEPhJLVv6QC8YZhC', 'chaskerd@sbwire.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20f9', 'Crosby', '$2a$04$RB0RZq2S78h8AGpCFuEAoOL7xvFOeNRI61EcwEnBAp1tGwE/RJ4GC', 'czammette@yahoo.co.jp');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20fa', 'Scarlet', '$2a$04$JmBIy3ofvhH/CI45l9LJ7e2nxoI5Jsy9RWbQw/DKXHpOx9aUCYRMO', 'seaglef@rediff.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20fb', 'Olvan', '$2a$04$AvFaqnEtcyPtNTD9kiCw4e/H6P2OuOhi603Utojqz6jZvM1eC3LuC', 'owaghorneg@google.co.uk');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20fc', 'Henrieta', '$2a$04$6CUr.QH/xCCtgHZYV1xq1Oa3sgFkHls9go2u.cfXe0WO2sSpXQFbe', 'htunaclifth@a8.net');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20fd', 'Padraig', '$2a$04$dcWoybOed2TjPGKcAd5E1.d21aCpnnt9ytDckcv8jcF9.jEWe.aiq', 'pkelletti@ox.ac.uk');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20fe', 'Kandy', '$2a$04$KCwVuB8Z9oE/rqkmMxO16Om2dKWfycjHAwBIEFclNNKPh9wKts5vq', 'kguerrij@google.pl');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa20ff', 'Silvan', '$2a$04$0XCIoaqWbvh4/sEAPlUQaOfSqMFkjdV./JDbRtOmMZfIJXc6PVoIu', 'sphilippk@chicagotribune.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2100', 'Cleopatra', '$2a$04$bFbaht8HQlfvJ6tGC5rHFeNbBm9Z6JqIsu.AGbZt7K1s05eacQ0Xe', 'cantonellil@merriam-webster.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2101', 'Electra', '$2a$04$7SezEGY.khASXbUMetxaJeLNj.yynbiqRB.TEI5hj0scFOqFFjyZe', 'ebouldingm@wisc.edu');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2102', 'Glenda', '$2a$04$uf9rXmcBFF3UxhEGWaHozel4qcoDPytXpw93QQxOXicChqPtWooo.', 'galderseyn@accuweather.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2103', 'Stanislas', '$2a$04$OwOXuTsf1zztA.HetDIrhO3MCBGD1gkjq33ODUxpPhBpyhYaMKpPu', 'sfrancklyno@techcrunch.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2104', 'Aila', '$2a$04$zFXQBgfk4hMzyiPp3YfVr.OuwD/JkW.h6dFI2OlJoOjE/dE4kClFe', 'ableadenp@odnoklassniki.ru');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2105', 'Cortney', '$2a$04$czqAwmN0npRojZjuZioL5O417PRLJOODyptNNto2mTb.6gWekKnxW', 'cmeanwellq@reference.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2106', 'Gwynne', '$2a$04$8a5UPYPpbJMxQ1rjBkRhseEu/FFJ6oPJ7jeVw13MvqPdsO0PRFVO2', 'glebelr@intel.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2107', 'Thurston', '$2a$04$fN7ZF2OSWshtAhTQGgoYsuAsyXtHIJeZMqWlRfKCg6V0TKoeRj80W', 'tfaloons@cafepress.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2108', 'Leese', '$2a$04$iY1meIHoNNcmEgyraikVKudpNTHp8PGXUm8HL8MSUYIOJICJ.Ao.u', 'lkeppelt@indiegogo.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2109', 'Barbabas', '$2a$04$xfc9/DLHWQqsMIN5dH7lu.jH.hafqKFP7Nd3vnC0cORoxS3x9Oy4i', 'blanstonu@dagondesign.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa210a', 'Donetta', '$2a$04$oTSYm8z2GsWT4bRfOSUN1ODoagTG4Yf9WLtkV1iM4TTKgO/pqTJfW', 'dwiggamv@angelfire.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa210b', 'Leilah', '$2a$04$MgilxGSXBAJ6pvDZ.DsDe.nGCk1pD.qQnUDm54yhg1yfouU5cM/Zy', 'lgiraudotw@rambler.ru');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa210c', 'Frederigo', '$2a$04$wPAxM1yiaPUjHaK0MouN7.G2u06EG25Q.TL2el2q3lExyYYIom5iu', 'fzannotellix@dropbox.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa210d', 'Ulrick', '$2a$04$e5KIOYUNU3l9O5xC38gWN.cChBQ/.eNbqFGHXvopArWQQ4q38klHK', 'udarkery@plala.or.jp');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa210e', 'Odilia', '$2a$04$g6aiaZ.EErQhZ1uEb0mrsOPxhyNN9zo2w.CWhRtwTllEro03oEBUW', 'osharplingz@elpais.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa210f', 'Nomi', '$2a$04$vyuJT30T6PAI.vFbqnOiF.OqlvCnH5jmBEV8s81Jxnem4DRSmdOIC', 'ntisun10@artisteer.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2110', 'Silvano', '$2a$04$OUILoQ91RlIZ3QOpn9XIxuHm.r26a0umz57dueBMWPWjHlC928m5C', 'sairton11@ucoz.ru');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2111', 'Merry', '$2a$04$EnmJIlRjiPsyBgEy6LprHe/KBtx8w4Tf9FjFJUHfsEH9g.YLnJY4W', 'mkettlestringe12@auda.org.au');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2112', 'Darice', '$2a$04$3LMJj/IFXwdi4ZqanItFIOQ3aq8vt4Mfwe.rwC/5zIJfyn3QvqAsq', 'diacofo13@merriam-webster.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2113', 'Melli', '$2a$04$DN5OfgKU.JT9J2c3M3K95.vmzeno9hcwtMNJO/v7halL5gvfmDQj2', 'mlilleycrop14@mysql.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2114', 'Glynda', '$2a$04$KZbZ4Aq8b3syqVyYcT.oPuM0XIgqRUhl5vL6I3tv19RFXsWrjiqTm', 'ggrealey15@discovery.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2115', 'Ginnie', '$2a$04$IBtcf0v4A83MaVvXCAREeuRB1uhntv5UhaSN0SYMic/ROVkaEpVpi', 'gbadman16@soundcloud.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2116', 'Tedie', '$2a$04$QxtBWTh5eKMiLYU1k7K4IeIUgSqt5HQp1kK2CrhEualQ29oOIrR8C', 'ttoffoloni17@theguardian.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2117', 'Babette', '$2a$04$ctdERfcYKMpEwv4PdUeuT.AiH1.6XNUZS.7I2/a1OiW6sMAUUDQuO', 'bmitkin18@reference.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2118', 'Deane', '$2a$04$wGgEYJrUz2njveRVf8jxHuGV.5SIrGCJttLu1nSMJ215zbkhdPrYm', 'degre19@businesswire.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa2119', 'Reamonn', '$2a$04$VHakyBhZ3ebEcnZy4LQo6.6.DJLLpZkBUZk.oFfuINIedIyLl1f6C', 'rstrelitzki1a@baidu.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa211a', 'Aigneis', '$2a$04$d/d3niuEAJIreq/OriUElOpDsMGIWQVjMahJHYwDBXVIqFcmXtz..', 'abygrave1b@nbcnews.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa211b', 'Marven', '$2a$04$6NIJKxav510sdRN1TseGverZzwxvoHMotZTnwMDR3uXZazpAH5OjO', 'mburet1c@hugedomains.com');
+insert into users (_id, username, password, email) values ('65757316fc13ae561bfa211c', 'Dorette', '$2a$04$RiqckewgZWBFj2jNYikcjeP.Jix16XQ3eRItgBaQ5tD0DfBH62B4G', 'drayner1d@deliciousdays.com');
--- user/src/database/postgres/postgres.ts
+++ user/src/database/postgres/postgres.ts
@@ -21,30 +21,30 @@ class Postgres implements Database {
   /* eslint-disable no-template-curly-in-string */
 
   async createEntity (data: any): Promise<any> {
-    return this.db.one('INSERT INTO user(${this:name}) VALUES(${this:csv}) RETURNING *', data)
+    return this.db.one(`INSERT INTO users  (username, password, email) VALUES ('${data.username}', '${data.password}', '${data.email}') RETURNING *`)
   }
 
   async getOneEntity (_id: string): Promise<any> {
-    return this.db.oneOrNone('SELECT * FROM user WHERE _id = $1', [_id])
+    return this.db.oneOrNone('SELECT * FROM users WHERE _id = $1', [_id])
   }
 
   async getOneEntityByUsername (username: string): Promise<any> {
-    return this.db.oneOrNone('SELECT * FROM user WHERE username = $1', [username])
+    return this.db.oneOrNone('SELECT * FROM users WHERE username = $1', [username])
   }
 
   async getAllEntities (params: Partial<any>): Promise<any> {
     const condition = Object.keys(params).map((key, index) => `$${index + 1}:${key}`).join(' AND ')
     const values = Object.values(params)
-    return this.db.manyOrNone(`SELECT * FROM user WHERE ${condition}`, values)
+    return this.db.manyOrNone(`SELECT * FROM users WHERE ${condition}`, values)
   }
   /* eslint-disable no-template-curly-in-string */
 
   async updateEntity (_id: string, newData: Partial<any>): Promise<any> {
-    return this.db.oneOrNone('UPDATE user SET ${this:name} WHERE _id = $1 RETURNING *', [_id, newData])
+    return this.db.oneOrNone('UPDATE users SET ${this:name} WHERE _id = $1 RETURNING *', [_id, newData])
   }
 
   async deleteEntity (_id: string): Promise<any> {
-    return this.db.oneOrNone('DELETE FROM user WHERE _id = $1 RETURNING *', [_id])
+    return this.db.oneOrNone('DELETE FROM users WHERE _id = $1 RETURNING *', [_id])
   }
 }
 
