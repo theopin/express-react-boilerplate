63dea59d81de62d88f3f63123c6d31de200b0fb4
theopin <theopinto98@hotmail.com>
2023-12-10 13:20:50 +0800

    Add sample data for users and add postgres impl in docker-compose

---
+++ data/users.sql
@@ -0,0 +1,57 @@
+create table users (
+	id INT,
+	username VARCHAR(50),
+	p_name VARCHAR(50),
+	email VARCHAR(50),
+	lang VARCHAR(50)
+);
+insert into users (id, username, p_name, email, lang) values (1, 'stryme0', 'Sasha', 'sbenford0@jalbum.net', 'Catalan');
+insert into users (id, username, p_name, email, lang) values (2, 'dskoate1', 'Dredi', 'dclemencon1@fema.gov', 'Kazakh');
+insert into users (id, username, p_name, email, lang) values (3, 'dbedells2', 'Dore', 'dpillinger2@fema.gov', 'Lao');
+insert into users (id, username, p_name, email, lang) values (4, 'msalling3', 'Michelina', 'msilverton3@mlb.com', 'Pashto');
+insert into users (id, username, p_name, email, lang) values (5, 'tmcwhan4', 'Tildie', 'tkindell4@time.com', 'Polish');
+insert into users (id, username, p_name, email, lang) values (6, 'clarcher5', 'Conni', 'crowcastle5@macromedia.com', 'Zulu');
+insert into users (id, username, p_name, email, lang) values (7, 'tfishly6', 'Tamarah', 'ttrevna6@microsoft.com', 'Persian');
+insert into users (id, username, p_name, email, lang) values (8, 'msuston7', 'Minta', 'mgreaser7@hc360.com', 'Thai');
+insert into users (id, username, p_name, email, lang) values (9, 'dcleef8', 'Darcey', 'dyosselevitch8@wordpress.com', 'Afrikaans');
+insert into users (id, username, p_name, email, lang) values (10, 'gguiver9', 'Gavra', 'gvasilov9@adobe.com', 'Tetum');
+insert into users (id, username, p_name, email, lang) values (11, 'rsilverwooda', 'Ruprecht', 'rnestora@gov.uk', 'Malayalam');
+insert into users (id, username, p_name, email, lang) values (12, 'abastoneb', 'Almeta', 'arennenbachb@dedecms.com', 'Finnish');
+insert into users (id, username, p_name, email, lang) values (13, 'calbersc', 'Clive', 'ckarpmanc@quantcast.com', 'Moldovan');
+insert into users (id, username, p_name, email, lang) values (14, 'njasikd', 'Nester', 'nharrodd@walmart.com', 'Marathi');
+insert into users (id, username, p_name, email, lang) values (15, 'honowlane', 'Hartwell', 'hcoleye@vinaora.com', 'Fijian');
+insert into users (id, username, p_name, email, lang) values (16, 'lbolingbrokef', 'Lyndsey', 'lcrannagef@devhub.com', 'New Zealand Sign Language');
+insert into users (id, username, p_name, email, lang) values (17, 'aseakingg', 'Ashien', 'aarmorg@cornell.edu', 'Belarusian');
+insert into users (id, username, p_name, email, lang) values (18, 'frosinah', 'Farris', 'fpierrepointh@mlb.com', 'Finnish');
+insert into users (id, username, p_name, email, lang) values (19, 'mgoddardi', 'Minette', 'mmallardi@wunderground.com', 'Gagauz');
+insert into users (id, username, p_name, email, lang) values (20, 'dgallawayj', 'Dominique', 'dmcconaghyj@china.com.cn', 'Irish Gaelic');
+insert into users (id, username, p_name, email, lang) values (21, 'rcaulierk', 'Rikki', 'rhatfieldk@csmonitor.com', 'Czech');
+insert into users (id, username, p_name, email, lang) values (22, 'jthornhilll', 'Jeane', 'jtomsettl@123-reg.co.uk', 'Tetum');
+insert into users (id, username, p_name, email, lang) values (23, 'lmackilpatrickm', 'Liuka', 'lebdenm@mysql.com', 'Korean');
+insert into users (id, username, p_name, email, lang) values (24, 'lnialsn', 'Leelah', 'lbennisonn@theguardian.com', 'West Frisian');
+insert into users (id, username, p_name, email, lang) values (25, 'mhadwicko', 'Marlin', 'malveyo@1688.com', 'Armenian');
+insert into users (id, username, p_name, email, lang) values (26, 'nantonomolip', 'Nicola', 'nstoodleyp@yahoo.co.jp', 'Kannada');
+insert into users (id, username, p_name, email, lang) values (27, 'sloftingq', 'Selma', 'seddinsq@google.co.uk', 'Somali');
+insert into users (id, username, p_name, email, lang) values (28, 'gortmannr', 'Gleda', 'glegonidecr@patch.com', 'Aymara');
+insert into users (id, username, p_name, email, lang) values (29, 'bmincis', 'Brita', 'bgazeys@cocolog-nifty.com', 'Assamese');
+insert into users (id, username, p_name, email, lang) values (30, 'amccuet', 'Armando', 'aguyet@salon.com', 'Tetum');
+insert into users (id, username, p_name, email, lang) values (31, 'kbrandoneu', 'Kipp', 'kbearwardu@sfgate.com', 'Fijian');
+insert into users (id, username, p_name, email, lang) values (32, 'dclimsonv', 'Dot', 'dsidnallv@senate.gov', 'Swati');
+insert into users (id, username, p_name, email, lang) values (33, 'ealsobrookw', 'Emma', 'espellicyw@cnn.com', 'Lao');
+insert into users (id, username, p_name, email, lang) values (34, 'larensonx', 'Lauren', 'lallisonx@icio.us', 'Mongolian');
+insert into users (id, username, p_name, email, lang) values (35, 'dgullefanty', 'Dwight', 'djellicoy@blogtalkradio.com', 'Gagauz');
+insert into users (id, username, p_name, email, lang) values (36, 'astubbingsz', 'Avrit', 'agebhardtz@jigsy.com', 'Tamil');
+insert into users (id, username, p_name, email, lang) values (37, 'lmccusker10', 'Lise', 'ldifrancecshi10@4shared.com', 'Arabic');
+insert into users (id, username, p_name, email, lang) values (38, 'csudy11', 'Chane', 'cdavidovitch11@1688.com', 'Polish');
+insert into users (id, username, p_name, email, lang) values (39, 'rwestwood12', 'Ravi', 'rswindells12@shutterfly.com', 'Ndebele');
+insert into users (id, username, p_name, email, lang) values (40, 'rarrol13', 'Royall', 'rpenticost13@skyrock.com', 'Latvian');
+insert into users (id, username, p_name, email, lang) values (41, 'lhorney14', 'Laney', 'lfowles14@printfriendly.com', 'Fijian');
+insert into users (id, username, p_name, email, lang) values (42, 'gwhitington15', 'Gerri', 'gturnock15@nhs.uk', 'Japanese');
+insert into users (id, username, p_name, email, lang) values (43, 'jcaro16', 'Jill', 'jbridgwood16@ehow.com', 'Zulu');
+insert into users (id, username, p_name, email, lang) values (44, 'ukennicott17', 'Ursola', 'ulamball17@craigslist.org', 'Hebrew');
+insert into users (id, username, p_name, email, lang) values (45, 'lneissen18', 'Lennie', 'lcrutch18@abc.net.au', 'Arabic');
+insert into users (id, username, p_name, email, lang) values (46, 'othomson19', 'Orlan', 'ostother19@howstuffworks.com', 'Afrikaans');
+insert into users (id, username, p_name, email, lang) values (47, 'aloukes1a', 'Angy', 'aorpen1a@ning.com', 'Irish Gaelic');
+insert into users (id, username, p_name, email, lang) values (48, 'bbabin1b', 'Boigie', 'bdansken1b@about.me', 'Lao');
+insert into users (id, username, p_name, email, lang) values (49, 'tgrunguer1c', 'Toddie', 'tmorrison1c@answers.com', 'Tamil');
+insert into users (id, username, p_name, email, lang) values (50, 'tmaddock1d', 'Tommy', 'tsummerton1d@apple.com', 'Swahili');
