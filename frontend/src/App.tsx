630bce45d1c4b3af52b09836e9affc160120d7cc
theopin <theopinto98@hotmail.com>
2023-12-23 13:15:45 +0800

    Update margin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { MainRouter } from './features/router/MainRouter'
 
 export function App (): JSX.Element {
   return (
-    <div className='mt-2 mr-2 ml-2'>
+    <div className='mt-2 mx-2'>
       <MainRouter />
     </div>
 
