ba625eb81bec04f3f8022f3fdabbca7ca4fffe41
theopin <theopinto98@hotmail.com>
2023-12-23 18:56:05 +0800

    Update pos of toast to be fixed

--- frontend/src/components/toasts/container/ToastContainer.tsx
+++ frontend/src/components/toasts/container/ToastContainer.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export function ToastContainer (): JSX.Element {
   return (
-        <div id='toastContainer' className="toast-container position-absolute top-0 end-0 p-3" />
+        <div id='toastContainer' className="toast-container position-fixed top-0 end-0 p-3" />
   )
 }
